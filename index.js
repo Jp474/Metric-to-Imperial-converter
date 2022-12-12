8a85cb816b288f4b7a03dc5fd8fd82d0740feacc
Jp474 <114600797+Jp474@users.noreply.github.com>
2022-12-12 15:46:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function length() {
     let meterVal = (resultInput.value / 3.281).toFixed(3)
     let feetVal = (resultInput.value * 3.281).toFixed(3)
 
-    if (resultInput.value === 0) {
+    if (Number(resultInput.value) === 0) {
         lengthPar.textContent = ""
-    }  else if (resultInput.value === 1) {
+    }  else if (Number(resultInput.value) === 1) {
         lengthPar.textContent = `${resultInput.value} meter = ${feetVal} feet | ${resultInput.value} foot = ${meterVal} meters`
     } else {lengthPar.textContent = `${resultInput.value} meters = ${feetVal} feet | ${resultInput.value} feet = ${meterVal} meters`
 }
@@ -34,9 +34,9 @@ function volume() {
     let literVal = (resultInput.value * 0.264).toFixed(3)
     let gallonVal = (resultInput.value / 0.264).toFixed(3)
 
-    if (resultInput.value === 1) {
+    if (Number(resultInput.value) === 1) {
         volumePar.textContent = `${resultInput.value} liter = ${literVal} gallons | ${resultInput.value} gallon = ${gallonVal} liters`
-    } else if (resultInput.value === 0) {
+    } else if (Number(resultInput.value) === 0) {
         volumePar.textContent = ""
     } else {volumePar.textContent = `${resultInput.value} liters = ${literVal} gallons | ${resultInput.value} gallons = ${gallonVal} liters`
 }
@@ -46,10 +46,10 @@ function mass() {
     let kilogramVal = (resultInput.value * 2.204).toFixed(3)
     let poundVal = (resultInput.value / 2.204).toFixed(3)
 
-    if (resultInput.value === 1) {
+    if (Number(resultInput.value) === 1) {
         massPar.textContent = `${resultInput.value} kilogram = ${kilogramVal} pounds | ${resultInput.value} pound = ${poundVal} kilograms`
-    } else if (resultInput.value === 0) {
+    } else if (Number(resultInput.value) === 0) {
         massPar.textContent = ""
-    } {massPar.textContent = `${resultInput.value} kilograms = ${kilogramVal} pounds | ${resultInput.value} pounds = ${poundVal} kilograms`
+    } else {massPar.textContent = `${resultInput.value} kilograms = ${kilogramVal} pounds | ${resultInput.value} pounds = ${poundVal} kilograms`
 }
 }
